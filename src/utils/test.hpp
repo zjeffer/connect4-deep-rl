c1b6a604290a13d2ff78f5de02a21c46d17b90f7
zjeffer <4633209+zjeffer@users.noreply.github.com>
2022-08-11 22:56:50 +0200

    feat(tests): added stochastic test and easy puzzle test

--- src/utils/test.hpp
+++ src/utils/test.hpp
@@ -1,8 +1,10 @@
+#include "selfPlaySettings.hpp"
 #include "../connect4/environment.hpp"
+#include "../game.hpp"
 #include "../common.hpp"
 
 
-void testHorizontalWin(){
+inline void testHorizontalWin(){
 	LOG(INFO) << "Testing horizontal win";
 	Environment env(6, 7);
 	env.makeMove(0);
@@ -17,7 +19,7 @@ void testHorizontalWin(){
 	assert(env.currentPlayerHasConnected4());
 }
 
-void testVerticalWin(){
+inline void testVerticalWin(){
 	LOG(INFO) << "Testing vertical win";
 	Environment env(6, 7);
 	env.makeMove(0);
@@ -31,7 +33,7 @@ void testVerticalWin(){
 	assert(env.currentPlayerHasConnected4());
 }
 
-void testDiagonalWin(){
+inline void testDiagonalWin(){
 	LOG(INFO) << "Testing diagonal win (from bottom left to top right)";
 	Environment env(6, 7);
 	env.makeMove(0);
@@ -62,4 +64,48 @@ void testDiagonalWin(){
 	env.makeMove(2);
 	env.makeMove(1);
 	assert(env.currentPlayerHasConnected4());
+}
+
+inline void testEasyPuzzle() {
+	SelfPlaySettings* settings = new SelfPlaySettings();
+	settings->addAgent("Yellow", "models/model.pt", ePlayer::YELLOW);
+	settings->addAgent("Red", "models/model.pt", ePlayer::RED);
+
+
+	settings->setSimulations(1000);
+	settings->setStochastic(false);
+	settings->setShowMoves(true);
+	settings->setSaveMemory(false);
+	Game game = Game(settings);
+
+
+	// for debugging purposes: try pre-configured board
+	std::vector<uint8_t> data = {
+		0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0,
+		0, 2, 2, 2, 0, 0, 0,
+		0, 1, 1, 1, 0, 0, 0,
+	};
+	auto options = torch::TensorOptions().dtype(torch::kInt8);
+	torch::Tensor board = torch::from_blob(data.data(), {settings->getRows(), settings->getCols()}, options);
+	game.getEnvironment()->setBoard(board); 
+	// game.getEnvironment()->setPlayer(ePlayer::RED);
+
+	assert(game.playGame() == ePlayer::YELLOW);
+}
+
+inline void testStochasticDistribution() {
+	std::vector<int> choices = {1000, 1, 2, 2, 2, 2, 1};
+	std::vector<int> counters = std::vector<int>(choices.size(), 0);
+	std::discrete_distribution<int> distribution(choices.begin(), choices.end());
+
+	for (int i = 0; i < 100000; i++) {
+		counters[distribution(g_generator)]++;
+	}
+
+	for (int i = 0; i < (int)counters.size(); i++) {
+		LOG(INFO) << "Choice " << i << ": " << counters[i];
+	}
 }
