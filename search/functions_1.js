d22948910dc71752cf2408cdd6647b3bd1cd83c6
zjeffer <4633209+zjeffer@users.noreply.github.com>
2022-11-15 21:04:20 +0000

    Deploying to gh-pages from  @ b0a4fdae7a7fb97492f4f2a13f5599f7ce1e10da 🚀

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['backpropagate_144',['backpropagate',['../class_m_c_t_s.html#a3a31c39fd67c62a0983eb72e3aabe1e6',1,'MCTS']]],
-  ['boardtoinput_145',['boardToInput',['../class_neural_network.html#af3080ac3ee98df639459890e3298ee59',1,'NeuralNetwork::boardToInput(torch::Tensor board, int player, int inputPlanes)'],['../class_neural_network.html#a021b6323cfc357894fb964e253a5bda9',1,'NeuralNetwork::boardToInput(std::shared_ptr&lt; Environment &gt; const &amp;env)']]]
+  ['boardtoinput_145',['boardToInput',['../class_neural_network.html#ae7b82cbc55bc9dece890c15041097019',1,'NeuralNetwork::boardToInput(torch::Tensor board, ePlayer player, int inputPlanes)'],['../class_neural_network.html#a021b6323cfc357894fb964e253a5bda9',1,'NeuralNetwork::boardToInput(std::shared_ptr&lt; Environment &gt; const &amp;env)']]]
 ];
